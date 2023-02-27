b95bb9a53af342483b89b428c5c027bb441ce305
feelinLit <podshivalov.o29@gmail.com>
2023-02-27 16:13:14 +0300

    fix: use tamplates for load time registering

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Res, UseInterceptors } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Render, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LoadTimeInterceptor } from './loadTime.interceptor';
 
@@ -9,23 +8,26 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  root(@Res() res: Response) {
-    return res.render('foodlog', {
+  @Render('foodlog')
+  root() {
+    return {
       authenticated: false,
-    });
+    };
   }
 
   @Get('calculator')
-  getCalculator(@Res() res: Response) {
-    return res.render('calculator', {
+  @Render('calculator')
+  getCalculator() {
+    return {
       authenticated: false,
-    });
+    };
   }
 
   @Get('tableoffoods')
-  getTableOfFoods(@Res() res: Response) {
-    return res.render('tableoffoods', {
+  @Render('tableoffoods')
+  getTableOfFoods() {
+    return {
       authenticated: false,
-    });
+    };
   }
 }
--- src/loadTime.interceptor.ts
+++ src/loadTime.interceptor.ts
@@ -5,22 +5,16 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class LoadTimeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const startTime = Date.now();
-    const response = context.switchToHttp().getResponse();
-    return next
-      .handle()
-      .pipe(
-        finalize(() =>
-          response.setHeader(
-            'X-Server-Load-Time',
-            `${Date.now() - startTime}ms`,
-          ),
-        ),
-      );
+    return next.handle().pipe(
+      map(() => {
+        return { serverLoadTime: `${Date.now() - startTime}ms` };
+      }),
+    );
   }
 }
